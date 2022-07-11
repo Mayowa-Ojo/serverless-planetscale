edf3e2d945fadfe14268a72fce9ebe04ba1dc835
Mayowa Ojo <46647640+Mayowa-Ojo@users.noreply.github.com>
2022-07-11 18:06:28 +0100

    fix: unhandled error

--- netlify/functions/albums.js
+++ netlify/functions/albums.js
@@ -1,10 +1,11 @@
-import client from "../../lib/db";
+import { getConnection } from "../../lib/db";
 
 export const handler = async function (event, ctx) {
-  const [rows] = await client.promise().query(`select * from albums;`);
+   const client = await getConnection();
+   const [rows] = await client.query(`select * from albums;`);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ data: rows }),
-  };
+   return {
+      statusCode: 200,
+      body: JSON.stringify({ data: rows }),
+   };
 };
